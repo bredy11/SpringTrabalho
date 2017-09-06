978715b8011bbe5ecc36e201cb4939bea2d6a8d4
rafael <rafael@rafael-PC>
2017-09-06 10:15:10 -0300

    livrar servidor

--- sonnya/src/main/java/br/com/sonnya/controller/ClienteController.java
+++ sonnya/src/main/java/br/com/sonnya/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sonnya.model.Comercio;
 import br.com.sonnya.service.ComercioService;
-
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 public class ClienteController {
 
@@ -22,7 +23,7 @@ public class ClienteController {
 	@Autowired
 	ComercioService comercioService;
 	
-	
+
 	@RequestMapping(method = RequestMethod.POST,value="/comercios",
 			produces=MediaType.APPLICATION_JSON_VALUE,
 			consumes=MediaType.APPLICATION_JSON_VALUE)
--- sonnya/src/main/java/br/com/sonnya/controller/UsuarioController.java
+++ sonnya/src/main/java/br/com/sonnya/controller/UsuarioController.java
@@ -3,20 +3,21 @@ package br.com.sonnya.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.sonnya.model.Comercio;
 import br.com.sonnya.model.Usuario;
-import br.com.sonnya.service.ComercioService;
 import br.com.sonnya.service.UsuarioService;
 
+@CrossOrigin
 @RestController
 public class UsuarioController {
 
@@ -34,13 +35,14 @@ public class UsuarioController {
 		return new ResponseEntity<Usuario>(usuarioService.alterarSerie(usuario),HttpStatus.CREATED);
 	}
 	
-	
+
 	@RequestMapping(method = RequestMethod.GET,value="/usuario",	
 			produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<Usuario>> BuscarTodos() {
-		
+		HttpHeaders headers = new HttpHeaders();
+	    headers.add("Access-Control-Allow-Origin", "http://localhost:3001");
 		Collection<Usuario> seriesColec = usuarioService.buscarTodasAsSeries(); 
-		return new ResponseEntity<>(seriesColec,HttpStatus.OK);
+		return new ResponseEntity<>(seriesColec,headers,HttpStatus.OK);
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT,value="/usuario",	
